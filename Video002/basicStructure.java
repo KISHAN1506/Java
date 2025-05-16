41ecd14f3bd441b7995850fc2036bce12f9b87dd
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-05-16 15:38:35 +0530

    Basic Defination and Structure of JAVA

---
+++ Video002/basicStructure.java
@@ -0,0 +1,9 @@
+public class basicStructure{
+    public static void main(String[] args) {
+        System.err.println("Hello World");
+    }
+    // public : make this accessible outside the class
+    // static : Can be called without creating an instance of the class.
+    // void : nothing to return
+    // main : program will start execution from here
+}
