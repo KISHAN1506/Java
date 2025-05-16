b1fca5aa727d6137d9c45de63bbafa63e45b222c
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-05-17 01:40:30 +0530

    Different Datatypes and Quick Quiz for addition of 3 numbers

---
+++ Video003/file.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class file{
+    public static void main(String[] args) {
+        // Mainly the data types are explained
+        // Quiz:
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
