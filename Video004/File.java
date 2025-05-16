0d6de87b8d7e0620f66234221bda3964b56d8fdc
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-05-17 02:01:57 +0530

    Literals in JAVA

---
+++ Video004/File.java
@@ -0,0 +1,18 @@
+public class File {
+    public static void main(String[] args) {
+        // Literals are these RHS ones
+        // like 34 , 56666 and so on
+        byte age = 34;
+        int age2 = 566666;
+        short age3 = 43;
+        long ageddd = 23334444444444l;
+        char ch = 'a';
+        float fl = 3.13f;
+        double db = 4.555;
+        boolean bl = true;
+        String str = "Kishan";
+        System.out.println(age);
+        System.out.println(bl);
+        System.out.println();
+    }
+}
