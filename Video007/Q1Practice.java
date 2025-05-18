56348f0d2bcaaaa17065ef84cc970125cfa85260
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-05-18 22:42:50 +0530

    Practice set 1

--- Video007/Q1Practice.java
+++ Video007/Q1Practice.java
@@ -7,6 +7,6 @@ public class Q1Practice {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
-        System.out.println(num1+num2+num3);
+        System.out.println(num1+num2+num3); 
     }
 }
