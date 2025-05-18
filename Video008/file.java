e4743c8d88ab74349e2675c36f356d2bc4e0bbdf
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-05-18 22:53:27 +0530

    Operators in JAVA

---
+++ Video008/file.java
@@ -0,0 +1,11 @@
+public class file {
+    public static void main(String[] args) {
+        // Types of operators:
+        // → Arithmetic Operators        → + , - , * , / , % , ++ , --
+        // → Assignment Operators        → = , +=
+        // → Comparison Operators        → == , != , > , >= , < , <=
+        // → Logical Operators           → && , || , !
+        // → Bitwise Operators           → & , |   (operates bitwise)
+
+    }
+}
