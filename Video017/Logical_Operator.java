19b88383c265eef2b7e38182142ebbb6b375f03c
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-01 20:36:46 +0530

    Logical Operator -> AND,OR,NOT, etc.

---
+++ Video017/Logical_Operator.java
@@ -0,0 +1,12 @@
+public class Logical_Operator {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        if(a && b){
+            System.out.println("Everthing TRUE!");
+        }else{
+            System.out.println("NOT Everthing TRUE!");
+        }
+    }
+}
