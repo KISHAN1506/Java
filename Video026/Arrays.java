49a9d1bd5d61ea0f860b42086102c8ee9b913f76
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-02 20:46:57 +0530

    Syntax to create Array

---
+++ Video026/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // 3 ways to create an ARRAY
+
+
+        // way1
+        // int[] arr = new  int[5];
+
+        // way2
+        // int[] arr2;
+        // arr2 = new int [100];
+
+        // way3
+        // int[] arr3 = { 100,50,33,43,12};
+    }
+}
