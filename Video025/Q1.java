12f7612ea1c15baf15e8f83f14dab09c1c8bfc92
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-02 20:36:04 +0530

    Practice Set for Looping

---
+++ Video025/Q1.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        for(int i = 0 ;i < val;i++){
+            for(int j = 0;j < val-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Video025/Q6.java
@@ -0,0 +1,7 @@
+public class Q6 {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("5");
+        }
+    }
+}
