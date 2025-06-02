3937823ccce0c3df3eee225f18d24914ad2e6121
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-02 19:56:15 +0530

    Do While loop and Quick Quiz

---
+++ Video022/do_while.java
@@ -0,0 +1,9 @@
+public class do_while {
+    public static void main(String[] args) {
+        int b = 3;
+        // Run ONCE without checking the CONDITION:
+        do { 
+            System.out.println(b);
+        } while (b<2);
+    }
+}
