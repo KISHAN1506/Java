150402f3093eaa4be892a2737ffa28204c433546
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-02 19:48:17 +0530

    Basics of Looping

---
+++ Video021/loop.java
@@ -0,0 +1,22 @@
+public class loop {
+    public static void main(String[] args) {
+        int j = 1;
+        while(j<=10){
+            System.out.println(j);
+            j++;
+        }
+
+
+        // Quick Quiz using for loop:
+        for(int i = 100;i<201;i++){
+            System.out.println(i);
+        }
+
+        // Quick Quiz using while loop:
+        int k = 100;
+        while(k<=200){
+            System.out.println(k);
+            k++;
+        }
+    }
+}
