4416e8fdc4bc9552d64acd3662688f49440d0307
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-02 23:35:31 +0530

    Basics of Methods in JAVA

---
+++ Video031/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    static float logic(float x,float y){
+        return (x+y)/5;
+    }
+    public static void main(String[] args) {
+        float a = 5,b = 7;
+        float ans = logic(a, b);
+        System.out.println(ans);
+
+    }
+}
