11d4fd0caf29f16fb9e10fb4da32a41fe4832753
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-06-10 16:22:51 +0530

    Basic Terminology of OOPs

---
+++ Video037/file.java
@@ -0,0 +1,11 @@
+public class file {
+    public static void main(String[] args) {
+        // Basic Terminology:
+
+        // 1. Abstraction
+        // 2. Encapsulation
+        // 3. Inheritance
+        // 4. Polymorphism
+        
+    }
+}
