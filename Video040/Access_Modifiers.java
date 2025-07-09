49a673155da704e6896cf0fe2e99f25f74680f63
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-07-09 21:54:56 +0530

    Access Modifier

---
+++ Video040/Access_Modifiers.java
@@ -0,0 +1,25 @@
+
+class MyEmployee {
+    private String name;
+    private int ID;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void getName() {
+        System.out.println(name);
+    }
+}
+
+public class Access_Modifiers {
+
+    public static void main(String[] args) {
+        MyEmployee kishan = new MyEmployee();
+        // kishan.name = "Kishan";
+        // kishan.ID = 1232;
+
+        // System.out.println("name = " + kishan.name + " and ID = " + kishan.ID);
+        kishan.setName("Kishan Agarwal");
+        kishan.getName();
+    }
+}
